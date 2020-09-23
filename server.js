509cdff4dbba2274fa0dba9a0c8b13261cb9ae5e
JT Salisbury <crazyscouter1@gmail.com>
2020-09-23 14:16:14 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/plex-web'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/plex-web/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});
